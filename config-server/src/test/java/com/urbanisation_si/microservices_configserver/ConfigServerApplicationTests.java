a09ef1f9b71f076851fddb8ededcfdb48be923b7
Jordan <petitjord@gmail.com>
2021-12-13 16:11:54 +0100

    urbanisation-si.clairprev.limiteNombreAssure=3

---
+++ config-server/src/test/java/com/urbanisation_si/microservices_configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanisation_si.microservices_configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
